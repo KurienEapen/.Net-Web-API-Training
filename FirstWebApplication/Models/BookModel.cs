765ce062e1853b5818d7355310a45274cfb1b794
KurienEapen <kurieneapenk@gmail.com>
2021-03-01 15:20:44 +0530

    added book model

---
+++ FirstWebApplication/Models/BookModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebApplication.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public int ReferenceId { get; set; }
+        public String Type { get; set; }
+        public int Pages { get; set; }
+        public float Price { get; set; }
+    }
+}
