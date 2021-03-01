46caf128bac88e05a04c87a36fd5124e61d7fda1
KurienEapen <kurieneapenk@gmail.com>
2021-03-01 14:47:47 +0530

    added controller

---
+++ FirstWebApplication/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstWebApplication.Controllers
+{
+    public class MessageController : ApiController
+    {
+        // GET: api/Message
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Message/5
+        public string Get(int id)
+        {
+            if(id < 10)
+            {
+                return "small value";
+            }
+            return "big value";
+        }
+
+        // POST: api/Message
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Message/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Message/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
